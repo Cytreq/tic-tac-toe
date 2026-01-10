714b7ab3b9a86a6a4ebccad45c9d3eed7896f3f3
Cyprian <bieglywybuch@gmail.com>
2026-01-10 16:36:47 +0100

    Initialize main menu and skeleton for PvP/settings

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <string>
 #include <ctype.h>
 /// board creation 
-void create_board(int n){
-    std::vector<std::vector<char>> board(n, std::vector<char>(n, ' '));
-    for(int i = 0; i < n; i++)
+int BoardSize = 5;
+std::vector<std::vector<char>> board(BoardSize, std::vector<char>(BoardSize, ' '));
+void print_board(){
+    std::cout << " ";
+    for(int j = 0; j < BoardSize; j++)
     {
-        for(int j = 0; j < n; j++)
+        char colHeader = 'A' + j;
+        std::cout << colHeader;
+    }
+    for(int i = 0; i < BoardSize; i++)
+    {
+        int rowHeader = 0 + i;
+        std::cout << rowHeader;
+        for(int j = 0; j < BoardSize; j++)
         {
             std::cout << board[i][j] << " ";
         }
@@ -23,7 +31,34 @@ bool is_digit(const std::string& s){
     }
     return true;
 }
+char parse_input()
+{
 
+}
+void play_pvp()
+{
+
+}
+void open_settings(){
+
+}
 int main(){
+    while(true)
+    {
+        std::string game_mode_str;
+        std::cout << "Wybierz tryb: \n 1. Gracz kontra Gracz \n 2. Ustawienia \n";
+        std::cin >> game_mode_str;
+        if(!is_digit(game_mode_str)) continue;
+        int game_mode = std::stoi(game_mode_str);
+        if(game_mode < 1 || game_mode > 2) continue;
+        switch (game_mode){
+            case 1:
+                play_pvp();
+            case 2:
+                open_settings();
+        }
+    }
+    print_board();
+    std::cin.get();
     return 0;
 }
